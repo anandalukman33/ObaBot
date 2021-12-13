c274018a51a7050896d34a697b8d3421bb93c904
Ananda Muhamad Lukman <ananda.muhamad.lukman@gmail.com>
2021-12-13 22:25:52 +0700

    Building Material Setup

---
+++ app/src/main/java/com/obasaka/obatbot/AboutObaBot.kt
@@ -0,0 +1,11 @@
+package com.obasaka.obatbot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutObaBot : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_oba_bot)
+    }
+}
---
+++ app/src/main/java/com/obasaka/obatbot/helper/HelperObaBot.kt
@@ -0,0 +1,12 @@
+package com.obasaka.obatbot.helper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.obasaka.obatbot.R
+
+class HelperObaBot : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_helper_oba_bot)
+    }
+}
